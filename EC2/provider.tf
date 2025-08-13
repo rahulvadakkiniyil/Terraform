35a71e11def857327aa26480babfd7b87ba97c90
Rahul Vadakkiniyil <rahulvadakkiniyil@gmail.com>
2025-08-13 10:10:57 +0530

    Update provider.tf

--- EC2/provider.tf
+++ EC2/provider.tf
@@ -1,3 +1,5 @@
-provider aws{
-    region = "ap-south-1"
+provider "aws" {
+    region = "Paste your region here"
+    access_key = "Paste your access key here"
+    secret_key = "Paste your secret key here"
 }
