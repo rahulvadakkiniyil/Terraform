7a2eeeabfe98c0ebb7c2b024104f6fcab9f36887
Rahul Vadakkiniyil <rahulvadakkiniyil@gmail.com>
2025-08-13 10:11:54 +0530

    Update providers.tf

--- features/Imports/providers.tf
+++ features/Imports/providers.tf
@@ -1,7 +1,6 @@
 provider "aws" {
     
-  region = "us-east-1"
-  access_key = "Paste your access key here"
-  secret_key = "Paste your secret key here"
-
+    region = "Paste your region here"
+    access_key = "Paste your access key here"
+    secret_key = "Paste your secret key here"
 }
